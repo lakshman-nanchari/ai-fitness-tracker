2f4aeaeb5aa138f37e7c41614e4055d4cd89ad3c
lakshman-nanchari <lakshmannanchari96@gmail.com>
2025-06-12 11:55:31 +0530

    Initial backend setup for AI Fitness Tracker

---
+++ backend/users/views.py
@@ -0,0 +1,8 @@
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def test_api(request):
+    return Response({"message": "User API is working!"})
