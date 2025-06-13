f33400f5348c262b320bf96b94d27c62ad38e527
Lakshman Kumar Nanchari <149482205+lakshman-nanchari@users.noreply.github.com>
2025-06-13 11:57:57 +0530

    Delete backend directory

--- backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('request-otp/', views.request_otp, name='request_otp'),
-    path('verify-otp/', views.verify_otp, name='verify_otp'),
-]
