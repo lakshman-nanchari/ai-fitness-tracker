ce2f02a6b96b9dd8e2a5e7188e80b2498ffb1fe4
lakshman-nanchari <lakshmannanchari96@gmail.com>
2025-06-24 12:48:11 +0530

    changing path name for meal plan

--- backend/mealplanner/urls.py
+++ backend/mealplanner/urls.py
@@ -3,6 +3,6 @@ from .views import MealPreferenceView, MealPlanListCreateView, MealPlanDeleteVie
 
 urlpatterns = [
     path("preferences/", MealPreferenceView.as_view(), name="meal-preferences"),
-    path("plans/", MealPlanListCreateView.as_view(), name="meal-plans"),
+    path("meal-plans/", MealPlanListCreateView.as_view(), name="meal-plans"),
     path("plans/<int:pk>/", MealPlanDeleteView.as_view(), name="delete-meal-plan"),
 ]
