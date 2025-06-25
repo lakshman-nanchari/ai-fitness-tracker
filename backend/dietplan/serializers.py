67a0e25ec83c3a3216d3f087d825c32ac0f9b269
lakshman-nanchari <lakshmannanchari96@gmail.com>
2025-06-25 16:52:39 +0530

    creating serializers for diet plan

---
+++ backend/dietplan/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import MealPreference, MealPlan
+
+class MealPreferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MealPreference
+        fields = '__all__'
+        read_only_fields = ['user']
+
+
+class MealPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MealPlan
+        fields = ['id', 'created_at', 'plan_text', 'user']
+        read_only_fields = ['id', 'created_at', 'user']
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep.pop('user', None)
+        return rep
