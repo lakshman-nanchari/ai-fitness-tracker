686a832e3b79e807e8d5cd88ae1c5611ef40bf22
lakshman-nanchari <lakshmannanchari96@gmail.com>
2025-06-25 16:58:18 +0530

    applied migrations to diet plan

--- backend/dietplan/admin.py
+++ backend/dietplan/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import MealPreference, MealPlan
 
-# Register your models here.
+@admin.register(MealPreference)
+class MealPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'diet_type', 'goal', 'calories_per_day')
+    search_fields = ('user__username', 'diet_type', 'goal')
+
+@admin.register(MealPlan)
+class MealPlanAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at')
+    search_fields = ('user__username',)
+    readonly_fields = ('created_at',)
